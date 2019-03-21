1de2026f136a25fbeb009b8f65c3a352b27d3e77
Nikhil Mahto <Nikhil@Nikhils-MacBook-Pro.local>
2019-03-21 15:07:46 +1100

    Change

--- tcs-service/src/lib/envVarsChecker.js
+++ tcs-service/src/lib/envVarsChecker.js
@@ -1,5 +1,5 @@
 export default env => {
-  const required = ['BASIQ_USER_TOKEN', 'COUNTRY', 'INSTITUTION_ID'];
+  const required = ['BASIQ_USER_TOKEN', 'COUNTRY'];
   const missing = [];
 
   required.forEach(reqVar => {
